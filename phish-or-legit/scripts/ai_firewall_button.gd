7b17a482e724b09e41ed603050c6e21e79b95c13
Slickomatic <slickomaticmc@gmail.com>
2026-04-13 12:06:14 -0400

    Added buy sound

--- phish-or-legit/scripts/ai_firewall_button.gd
+++ phish-or-legit/scripts/ai_firewall_button.gd
@@ -3,6 +3,8 @@ extends Button
 @export var cost: int = 150
 @export var power_up_name: String = "AI Firewall"
 
+@onready var buy_sound = $"../BuySound"
+
 func _ready() -> void:
 	pressed.connect(_on_pressed)
 
@@ -10,6 +12,7 @@ func _on_pressed() -> void:
 	if GameManager.salary >= cost:
 		GameManager.salary -= cost
 		print("Bought:", power_up_name)
+		buy_sound.play()
 		# Add your power-up activation logic here
 	else:
 		print("Not enough money!")
